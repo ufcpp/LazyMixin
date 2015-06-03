f2b585163ab1a98408a1c2ae475e2450545a9215
niwanaga <iwanaga@orange-cube.jp>
2015-06-03 18:32:09 +0900

    DoNotUseLazyForProperty

---
+++ LazyMixin/LazyMixinAnalyzer/LazyMixinAnalyzer.Test/DoNotUseLazyForPropertyUnitTests.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestHelper;
+
+namespace LazyMixinAnalyzer.Test
+{
+    [TestClass]
+    public class DoNotUseLazyForPropertyUnitTest : CodeFixVerifier
+    {
+        [TestMethod]
+        public void NoDiagnositcs()
+        {
+            var test = @"
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Laziness;
+
+namespace ConsoleApplication1
+{
+    class TypeName
+    {
+        public StringBuilder X => _x.Value;
+        private LazyMixin<StringBuilder> _x;
+    }
+}";
+
+            VerifyCSharpDiagnostic(test);
+        }
+
+        [TestMethod]
+        public void DoNotUseLazyForProperty()
+        {
+            var test = @"
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Laziness;
+
+namespace ConsoleApplication1
+{
+    class TypeName
+    {
+        public LazyMixin<StringBuilder> X { get; }
+    }
+}";
+            var expected = new DiagnosticResult
+            {
+                Id = DoNotUseLazyForPropertyAnalyzer.DiagnosticId,
+                Message = string.Format(DoNotUseLazyForPropertyAnalyzer.MessageFormat, "X"),
+                Severity = DiagnosticSeverity.Error,
+                Locations =
+                    new[]
+                    {
+                        new DiagnosticResultLocation("Test0.cs", 14, 9)
+                    }
+            };
+
+            VerifyCSharpDiagnostic(test, expected);
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new DoNotUseLazyForPropertyAnalyzer();
+        }
+    }
+}
---
+++ LazyMixin/LazyMixinAnalyzer/LazyMixinAnalyzer/DoNotUseLazyForProperty/DoNotUseLazyForPropertyAnalyzer.cs
@@ -0,0 +1,39 @@
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace LazyMixinAnalyzer
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class DoNotUseLazyForPropertyAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "DoNotUseLazyForProperty";
+        internal const string Title = "Do not use LazyMixin<T> for a property.";
+        internal const string MessageFormat = "The type of the property '{0}' is LazyMixin<T>, which is not allowed.";
+        internal const string Category = "Correction";
+
+        internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Error, isEnabledByDefault: true);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSymbolAction(AnalyzePropertyDeclaration, SymbolKind.Property);
+        }
+
+        private void AnalyzePropertyDeclaration(SymbolAnalysisContext context)
+        {
+            var p = (IPropertySymbol)context.Symbol;
+            var t = p.Type;
+
+            if (t.ContainingNamespace.Name == "Laziness" && t.MetadataName == "LazyMixin`1")
+            {
+                var node = p.DeclaringSyntaxReferences.First().GetSyntax();
+                var diagnostic = Diagnostic.Create(Rule, node.GetLocation(), p.Name);
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+    }
+}
